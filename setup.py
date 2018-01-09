0eca6d1dbb2aa0018d4265fb2b813cdf1b800cf0
Eric Marshall <emm035@bucknell.edu>
2018-01-08 17:08:46 -0800

    Added yml support

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pmake',
+  packages = ['pmake'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A make-inspired build script',
+  author = 'Eric Marshall',
+  author_email = 'ericmarshall715@gmail.com',
+  url = 'https://github.com/emm035/pmake',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
