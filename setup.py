b8018978ff5e1ba3b98d797506ecf6ddfbc24076
Eric Marshall <emm035@bucknell.edu>
2018-03-30 14:14:30 -0400

    Add changes

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pmake',
+  packages = ['pmake'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A python build script with support for custom output and parallel builds',
+  author = 'Eric Marshall',
+  author_email = 'ericmarshall715@gmail.com',
+  url = 'https://github.com/emm035/pmake', # use the URL to the github repo
+  download_url = 'https://github.com/emm035/pmake/archive/1.0.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
