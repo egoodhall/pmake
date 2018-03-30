abe739310a2e9400ef8e9e3d7ce8e885e5993253
Eric Marshall <emm035@bucknell.edu>
2018-03-30 15:43:15 -0400

    Backported log directory creation

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'pmake',
   packages = ['pmake'], # this must be the same as the name above
-  version = '1.0.7',
+  version = '1.0.8',
   description = 'A python build script with support for custom output and parallel builds',
   author = 'Eric Marshall',
   author_email = 'ericmarshall715@gmail.com',
   url = 'https://github.com/emm035/pmake', # use the URL to the github repo
-  download_url = 'https://github.com/emm035/pmake/archive/1.0.7.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/emm035/pmake/archive/1.0.8.tar.gz', # I'll explain this in a second
   keywords = ['build', 'logging', 'parallel'], # arbitrary keywords
   classifiers = [],
   scripts = ['pmake/pmake'],
